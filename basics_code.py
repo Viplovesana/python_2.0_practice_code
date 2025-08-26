5a48759e48efe2d16bc0bfbfe3a9b5950ea8ecd9
Viplovesana <viplovesana90@gmail.com>
2025-08-26 18:07:48 +0530

    new commit

--- basics_code.py
+++ basics_code.py
@@ -22,3 +22,13 @@ print("tupe of a is ",type(a))
 print("tupe of b is ",type(b))
 print("tupe of c is ",type(c))
 print("tupe of d is ",type(d))
+
+
+# ............................datatype............................
+
+list1 = [1,3,5.6,['viplove'],["apple ,banana"]]
+print(list1)
+tuple1 = (1,3,5.6,('viplove'),("apple ,banana"))
+print(tuple1)
+dict1 = {"name":"viplove","age":23,"city":"dewas"}
+print(dict1)
