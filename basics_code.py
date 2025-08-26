306e40c30ec213327fc8a6e96af373ccc87a32b0
Viplovesana <viplovesana90@gmail.com>
2025-08-26 20:01:23 +0530

    python

--- basics_code.py
+++ basics_code.py
@@ -12,57 +12,68 @@
 
 # ............variables......................................................................
 
-a = 1 
-b = "viplove sana"
-c = True
-d = None
-print(a)
-print(b)
-print("tupe of a is ",type(a))
-print("tupe of b is ",type(b))
-print("tupe of c is ",type(c))
-print("tupe of d is ",type(d))
+# a = 1 
+# b = "viplove sana"
+# c = True
+# d = None
+# print(a)
+# print(b)
+# print("tupe of a is ",type(a))
+# print("tupe of b is ",type(b))
+# print("tupe of c is ",type(c))
+# print("tupe of d is ",type(d))
 
 
 # ............................datatype............................
 
-list1 = [1,3,5.6,['viplove'],["apple ,banana"]]
-print(list1)
-tuple1 = (1,3,5.6,('viplove'),("apple ,banana"))
-print(tuple1)
-dict1 = {"name":"viplove","age":23,"city":"dewas"}
-print(dict1)
+# list1 = [1,3,5.6,['viplove'],["apple ,banana"]]
+# print(list1)
+# tuple1 = (1,3,5.6,('viplove'),("apple ,banana"))
+# print(tuple1)
+# dict1 = {"name":"viplove","age":23,"city":"dewas"}
+# print(dict1)
 
 # .............................operators..........................
 
-print(5+6)          
-print(15-6)
-print(15*6)
-print(15/6)
-print(15//6)
-print(15%6)
-print(5**6)
+# print(5+6)          
+# print(15-6)
+# print(15*6)
+# print(15/6)
+# print(15//6)
+# print(15%6)
+# print(5**6)
 
 # .................................calculator...........................
 
-operator = input("Enter the operator to calculate ")
-number1 = int(input("Enter the 1st value to calculate "))
-number2 = int(input("Enter the 2nd value to calculate "))
+# operator = input("Enter the operator to calculate ")
+# number1 = int(input("Enter the 1st value to calculate "))
+# number2 = int(input("Enter the 2nd value to calculate "))
 
-if operator == "+":
-    print("result : ",number1+number2 )
-elif operator == "-":
-    print("result : ",number1-number2 )
-elif operator == "*":
-    print("result : ",number1*number2 )
-elif operator == "/":
-    if number1 or number1 == 0:
-        print("cants divisble by zero ")
-    else:
-        print("result : ",number1/number2 )    
-else:
-    print("invalid")
+# if operator == "+":
+#     print("result : ",number1+number2 )
+# elif operator == "-":
+#     print("result : ",number1-number2 )
+# elif operator == "*":
+#     print("result : ",number1*number2 )
+# elif operator == "/":
+#     if number1 or number1 == 0:
+#         print("cants divisble by zero ")
+#     else:
+#         print("result : ",number1/number2 )    
+# else:
+#     print("invalid Operator")
 # ......................................end.......................................
+
+# ....................................type casting or converssion........................
+
+a = "5"
+b = "8"
+# the conversion of one datatype into another datatype iss called typecasting inn python this is
+print (int(a)+int(b)) 
+c = 9.8
+d = 9
+print(c+d)
+
 
 
 
