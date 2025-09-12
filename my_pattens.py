c2dc05a9d0badd423c743b649c45712867a3b2c9
Viplovesana <viplovesana90@gmail.com>
2025-09-12 17:42:35 +0530

    new commit

--- my_pattens.py
+++ my_pattens.py
@@ -33,12 +33,30 @@ for i in range(n,0,-1):
 
 
 # wap to make a  right triangle and invert and invert of it 
-n = int(input("Enter the no :"))
+'''n = int(input("Enter the no :"))
 
 for i in range(0,n + 1):
     print(""*(n-i),"A"*i)
 
 for i in range(n,0,-1):
-    print(""*(n-i),"A"*i)    
+    print(""*(n-i),"A"*i)  ''' 
+
+
+#WAP....... 
+#        * 
+#       ** 
+#      *** 
+#    **** 
+#  ***** 
+# ***** 
+# **** 
+# *** 
+# ** 
+# * 
+n = int(input("Enter the num :"))
+for i in range(1,n+1):
+    print(" "*(n-i)+"*"*i)
+for i in range(n,0,-1):
+    print(""*(n-i)+"*"*i) 
     
    
