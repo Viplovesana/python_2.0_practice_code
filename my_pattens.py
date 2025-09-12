dc51b461ec2ec6da9ae7f1fe629bc5e15223bee9
Viplovesana <viplovesana90@gmail.com>
2025-09-12 17:51:50 +0530

    new commit

--- my_pattens.py
+++ my_pattens.py
@@ -67,5 +67,5 @@ for i in range(n,0,-1):
 #       * 
 n = int(input("Enter the num :"))
 for i in range(n,0,-1):
-    print(" "*(n-i)+"*"*(2*i-1))
-    # print(""*(n-i)+"*"*i)
+    # print(" "*(n-i)+"*"*(2*i-1))
+    print(" "*(n-i)+"* "*i)
