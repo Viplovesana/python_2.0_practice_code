7faca0ba952512498354499f0d361a5d1a95d3ce
Viplovesana <viplovesana90@gmail.com>
2025-09-13 14:28:36 +0530

    new commit

--- my_pattens.py
+++ my_pattens.py
@@ -171,7 +171,18 @@ for i in range(1,n+1):
     print() '''
     
 
-  
+# WAp........
+#         1 
+#        1 2 
+#       1 2 3
+#      1 2 3 4
+#     1 2 3 4 5 
+n = int(input("Enter the no :"))
+for i in range (1,n+1):
+    print(" "*(n-i),end="")
+    for j in range (1,i+1):
+        print(j,end=" ")   
+    print()    
 
 
 
