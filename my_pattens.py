acfdb5ac613ae76348331f9c79a1c57ab88e5789
Viplovesana <viplovesana90@gmail.com>
2025-09-13 14:25:30 +0530

    new commit

--- my_pattens.py
+++ my_pattens.py
@@ -144,3 +144,19 @@ for i in range(1,n+1):
         print(ch,end="")
         ch = chr(ord(ch)+1)
     print()  '''    
+
+#  WAP ......
+# 2  
+# 2 4 
+# 2 4 6 
+# 2 4 6 8 
+# 2 4 6 8 10 
+'''n = int(input("Enter the no :"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(2*j,end="")  
+    print() '''
+
+
+
+
