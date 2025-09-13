d501105681d8a251e4dfee7f67efa4ce6697d54b
Viplovesana <viplovesana90@gmail.com>
2025-09-13 14:24:14 +0530

    new commit

--- my_pattens.py
+++ my_pattens.py
@@ -89,10 +89,48 @@ for i in range(1,n+1):
 # 1 2 3 
 # 1 2 3 4 
 # 1 2 3 4 5
-n = int(input("Enter the num :"))
+'''n = int(input("Enter the num :"))
 for i in range(1,n+1):
     for j in range(1,i+1):
         print(j,end="")
-    print()    
+    print()  '''  
+# WAP.........
+# 1 2 3 4 5  
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 5
+'''n = int(input("Enter the num :"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(j,end="")
+    print() '''   
+# WAP...............
+# 1    
+# 1 2  
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5
+'''n = int(input("Enter the num :"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print() '''   
+
+# WAP ...
+# A B C D E  
+# A B C D E 
+# A B C D E 
+# A B C D E 
+# A B C D E
+'''n = int(input("Enter the num :"))
+for i in range(1,n+1):   
+    ch = "A"
+    for j in range(1,n+1):
+        print(ch,end="")
+        ch = chr(ord(ch)+1) 
+    print()  '''
+
+
 
 
