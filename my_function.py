ca653d11ab660fc68a67b0f9b04f9064e12fd9be
Viplovesana <viplovesana90@gmail.com>
2025-09-20 15:45:56 +0530

    new commit

--- my_function.py
+++ my_function.py
@@ -186,6 +186,22 @@ Iteration 3:
 last_digit = 1 % 10 = 1
 total = 152 + 1**3 = 153
 temp = 1 // 10 = 0'''
+
+
+# Example 11: Python program to count the number of even and odd numbers from a series of numbers using functions.
+n = [1,2,3,4,5,6,7,8,9,22,34,65,76,87,23,83,686]
+def odd_even(num):
+    odd_count = 0
+    even_count = 0
+    for i in num:
+        if i % 2 == 0 :
+            even_count = even_count + 1        
+        else:
+            odd_count = odd_count + 1                  
+    print(even_count,"is the total count of even no")
+    print(odd_count,"is the total count of odd no") 
+   
+odd_even(n)
     
     
     
