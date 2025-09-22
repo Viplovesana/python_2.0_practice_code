6e68cdcaf13aeefa3e589be938942b202014e6e3
Viplovesana <viplovesana90@gmail.com>
2025-09-23 01:02:35 +0530

    new commit

--- my_function/heigher_order_funct.py
+++ my_function/heigher_order_funct.py
@@ -10,13 +10,19 @@ print = lam_daa(5)'''
 
 # /So this is the lamdaa function use in the single line 
 '''double = lambda x: x * 2  
-print(double(6))'''
+print(double(6))
 
-'''triples = lambda x : x * 3
+triples = lambda x : x * 3
 print(triples(5))'''
 
-'''triples = lambda x : x * x * x
-print(triples(5))'''
+cube = lambda x : x * x * x
+print(cube(5))
 
 average  = lambda x ,y,z :(x+y+z)/3 
 print(average(20,30,50))
+
+def apple(fx,value): 
+    return 2 + fx(value)
+
+result = (apple(lambda x : x * x * x,2))
+print (result)
