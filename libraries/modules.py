690ad0eaab31a9a00e5de988899dfd4235493688
Viplovesana <viplovesana90@gmail.com>
2025-09-24 00:55:18 +0530

    new commit

---
+++ libraries/modules.py
@@ -0,0 +1,4 @@
+import mymodule
+
+mymodule.say_hello("viplove")
+mymodule.say_bye("viplove")
---
+++ libraries/mymodule.py
@@ -0,0 +1,7 @@
+
+
+def say_hello(name):
+    print(f"welcom,{name}!")
+
+def say_bye(name):
+    print(f"bye,{name}!")    
