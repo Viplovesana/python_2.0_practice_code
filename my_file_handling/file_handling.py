aec2a4e62e8810b3e411facb70b99374df216a0f
Viplovesana <viplovesana90@gmail.com>
2025-09-24 11:51:43 +0530

    new commit

--- my_file_handling/file_handling.py
+++ my_file_handling/file_handling.py
@@ -15,13 +15,25 @@ file.close()
 
 file = open('example2.txt','w')
 
-content = file.write("hey jhon this is viplove on board, where are you")
+content = file.write("hey jhon this is viplove on board, ")
 print(content)
 file.close()   
+
+file = open('example2.txt','w')
+
+content = file.write("where are you jhon, ")
+print(content)
+file.close() 
 
 # ...append mode......
 file = open('example2.txt','a')
 
 content = file.write("\nyaa i am on the way ")
 print(content)
-file.close()   
+file.close() 
+
+file = open('example2.txt','a')
+
+content = file.write("\nplease wait ")
+print(content)
+file.close()  
