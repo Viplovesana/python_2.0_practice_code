3c733bb50d4c75f716012117b79ed6af9e53927c
Viplovesana <viplovesana90@gmail.com>
2025-09-24 11:27:25 +0530

    new commit

--- my_file_handling/file_handling.py
+++ my_file_handling/file_handling.py
@@ -5,9 +5,16 @@
 
 # ....read mode.........
 
-file = open('example.txt','r')
+'''file = open('example.txt','r')
 
 content = file.read()
 print(content)
 file.close()
+'''
+# ....write mode........
 
+file = open('example2.txt','w')
+
+content = file.read()
+print(content)
+file.close()
