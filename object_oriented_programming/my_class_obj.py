40ba679052cc98fdf86aacd0d564012913a93b15
Viplovesana <viplovesana90@gmail.com>
2025-09-25 04:05:04 +0530

    new commit

--- object_oriented_programming/my_class_obj.py
+++ object_oriented_programming/my_class_obj.py
@@ -4,7 +4,7 @@
     print("viplove")
 Student()    '''
 
-class Student:
+'''class Student:
 
     def __init__(self):
         print("this is my default constructor")
@@ -17,5 +17,26 @@ s1 = Student("viplove sana",24)
 print(s1.name,s1.age)
 
 s2 =Student("piyush bhai",22)
-print(s2.name,s2.age)
+print(s2.name,s2.age)'''
+
+
+class Student:
+    def __init__(self,fullname,myage):
+        self.name = fullname
+        self.age = myage
+
+    def say_hello(self):
+        print("hello",self.name)
+
+    def say_bye(self):
+        return self.name     
+
+s1 = Student("viplove",24)
+print (s1.name,s1.age)
+
+s1.say_hello()
+print(s1.say_bye())
+
+
+
  
